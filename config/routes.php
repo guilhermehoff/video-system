9d3cee066daa5c3f42ad4b55dddbd9a0edda71bf
guilhermehoff <guilherme_rossi_hoff@hotmail.com>
2023-05-07 16:32:14 -0300

    create controller API

--- config/routes.php
+++ config/routes.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 use Alura\Mvc\Controller\DeleteVideoController;
 use Alura\Mvc\Controller\EditVideoController;
+use Alura\Mvc\Controller\JsonVideoListController;
 use Alura\Mvc\Controller\LoginController;
 use Alura\Mvc\Controller\LoginFormController;
 use Alura\Mvc\Controller\LogoutController;
+use Alura\Mvc\Controller\NewJsonVideoController;
 use Alura\Mvc\Controller\NewVideoController;
 use Alura\Mvc\Controller\VideoFormController;
 use Alura\Mvc\Controller\VideoListController;
@@ -21,5 +23,7 @@ return [
     'GET|/login' => LoginFormController::class,
     'POST|/login' => LoginController::class,
     'GET|/logout' => LogoutController::class,
+    'GET|/videos-json' => JsonVideoListController::class,
+    'POST|/videos' => NewJsonVideoController::class,
 
 ];
